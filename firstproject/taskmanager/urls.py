a966fe11b8cdc4440bd598470ce0cfa339e96351
Alina Shirikhanova <alina.meller.98@mail.ru>
2023-01-14 13:25:01 +0300

    Lesson30

--- firstproject/taskmanager/urls.py
+++ firstproject/taskmanager/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from taskmanager.views import index
+from taskmanager.views import index, by_category
 
 urlpatterns = [
-    path('', index),
+    path('<int:category_id>/', by_category),
+    path('', index, name='index'),
 ]
 
 # репозиторий - папка
--- firstproject/taskmanager/views.py
+++ firstproject/taskmanager/views.py
@@ -2,9 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from taskmanager.models import Task
+from taskmanager.models import Task, Category
 
 
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'taskmanager/index.html', {'tasks': tasks})
+
+
+def by_category(request, category_id):
+    tasks = Task.objects.filter(category=category_id)
+    categories = Category.objects.all()
+    current_category = Category.objects.get(pk=category_id)
+    context = {'tasks': tasks, 'categories': categories, 'current_category': current_category}
+    return render(request, 'taskmanager/by_category.html', context)
